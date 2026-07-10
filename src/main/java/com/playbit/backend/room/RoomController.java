2ed604b44998dbf1040b843fb8e321e558d6b16d
soohyungit <soohyun5576@gmail.com>
2026-07-10 15:54:27 +0900

    refactor: api 엔드포인트 추가

--- src/main/java/com/playbit/backend/room/RoomController.java
+++ src/main/java/com/playbit/backend/room/RoomController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/rooms")
+@RequestMapping("/api/rooms")
 @RequiredArgsConstructor
 
 public class RoomController {
