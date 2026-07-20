5d2d6046ed35cb06620f2a258a4048f2cf033380
zzulYH-11 <jjulu0227@naver.com>
2026-07-20 17:41:32 +0900

    test: MemberController 단위 테스트 작성

---
+++ src/test/java/com/playbit/backend/member/MemberControllerTest.java
@@ -0,0 +1,48 @@
+package com.playbit.backend.member;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(MemberController.class)
+public class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private MemberService memberService;
+
+    @MockitoBean
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("사용자에게 uuid를 발급하고 성공적으로 등록한다.")
+    void createMember_success() throws Exception {
+
+        //given
+        UUID uuid = UUID.randomUUID();
+        MemberDTO mockResponse = new MemberDTO(uuid);
+        given(memberService.createMember()).willReturn(mockResponse);
+
+        //when & then
+        mockMvc.perform(post("/api/members")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.data.uuid").value(uuid.toString()))
+                .andExpect(header().exists("location"));
+
+    }
+
+}
