6a1f3b84193c07be5f4573bd4cf92bab4139d5c4
zzulYH-11 <jjulu0227@naver.com>
2026-08-18 03:02:46 +0900

    feat: 비관적 락을 적용하여 스트릭 로직 동시성 문제 해결

--- src/main/java/com/playbit/backend/member/MemberRepository.java
+++ src/main/java/com/playbit/backend/member/MemberRepository.java
@@ -1,6 +1,10 @@
 package com.playbit.backend.member;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +16,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByNickname(String nickname);
 
     List<Member> findAllByMemberIdIn(List<Long> memberId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT m FROM Member m WHERE m.memberId = :memberId")
+    Optional<Member> findByIdWithPessimisticLock(@Param("memberId") Long memberId);
 }
